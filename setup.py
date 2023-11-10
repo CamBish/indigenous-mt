43e7e45c6a6e84bfa00cfd94a328c9c66c0a97f2
CamBish <camb712@gmail.com>
2023-11-09 23:48:13 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Testing ChatGPT and other LLMs\' capabilities to translate Canadian Indigenous Languages, such as Inuktitut and Cree',
+    author='Cameron Bishop',
+    license='MIT',
+)
